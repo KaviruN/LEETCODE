7b42c74fcccf01320a87c4b4284c68656717e270
Kaviru <kavirunethsara1@outlook.com>
2025-09-27 18:29:37 +0530

    Fix input string for longest substring algorithm and clean up variable initialization

--- Longest Substring Without Repeating Characters/main.py
+++ Longest Substring Without Repeating Characters/main.py
@@ -1,15 +1,20 @@
-s = "pwwkew"
+s = "pww"
 a = [] #"p", "w", "w", "k", "e", "w"
 # s = "pw"
 
-left,right = []
+left = []
+right = []
+
+maxlen = 0
 
 for i in range(len(s)):
     if s[i] in a:
         left.append(s[:i])
         right.append(s[i:])
-        print(left,right)
+        print(left, right)
+        
     else:
         a.append(s[i])
+        maxlen += 1
 
 
