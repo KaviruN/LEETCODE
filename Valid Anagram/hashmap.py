13ccaab04fd8ef4fcabd6d8d67b229b83c6686a3
Kaviru <kavirunethsara1@outlook.com>
2025-09-28 20:10:48 +0530

    Implement initial structure for isAnagram function and prepare character count dictionaries

--- Valid Anagram/hashmap.py
+++ Valid Anagram/hashmap.py
@@ -1,16 +1,18 @@
 s = "anagram"
 t = "nagacam"
 
-# def isAnagram(s, t):
-#     sCount, tCount = {}, {}
+def isAnagram(s, t):
+    sCount, tCount = {}, {}
 
-#     for i in range(len(s)):
-#         sCount[s[i]] = 
+    for i in range(len(s)):
+        sCount[s[i]] += 1
+
+    print(sCount)  
 
 
-a = {}
+# a = {}
 
 
-a['a'] = 1
+# a['a'] = 1
 
-print(a)
+# print(a)
