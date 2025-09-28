e47053ac88853d65f2784e43888bf3d8731b3b4d
Kaviru <kavirunethsara1@outlook.com>
2025-09-28 12:10:01 +0530

    Update Contains Duplicate algorithm to handle input and improve logic

--- Contains Duplicate/main.py
+++ Contains Duplicate/main.py
@@ -1,12 +1,14 @@
-nums = [1,2,3,1]
+nums = [1, 2, 3, 2, 1]
 
 nums = sorted(nums)
 
-print(nums)
+state = False
 
-def ConDup():
-    for i in range(len(nums) + 1):
-        if nums[i] in nums[i + 1]:
-            return True
-        else:
-            return False
+
+for i in range(len(nums) - 1):
+    if not state and nums[i] == nums[(i + 1)]:
+        state = True
+
+        
+
+print(state)
