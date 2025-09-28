6f8d9ee3c6c637ab792bed44a85c5c26c527adb3
Kaviru <kavirunethsara1@outlook.com>
2025-09-28 10:42:44 +0530

    Refactor longest substring algorithm to improve logic and update input string

--- Longest Substring Without Repeating Characters/main.py
+++ Longest Substring Without Repeating Characters/main.py
@@ -1,36 +1,25 @@
-s = "abcabcaa"
-a = [] #"p", "w", "w", "k", "e", "w"
-# s = "pw"
 
+
+s = "bbbbb"
+
+a = []       
 left = 0
-right = 0
-
 maxlen = 0
 
-for i in range(len(s)):
-    if s[i] in a:
-        a = []
-        a.append(s[i])
-        right += 1
-    elif maxlen < len(a):
-        maxlen = len(a)
+for right in range(len(s)):
+   
+    while s[right] in a:
+        a.pop(0)  
+        left += 1
     
-    a.append(s[i])
+    a.append(s[right])  
+    maxlen = max(maxlen, len(a))  
 
-# while right < len(s):
-#     for i in range(len(s)):
-#         if s[i] in a:
-#             left = i - 1
-#             right = i
-#             print(s[left:right])
-#         a.append(s[i])
+print(maxlen)  
 
-    
 
-# print(s[6:6])
 
-print(maxlen)
-        
+
 
 
 
