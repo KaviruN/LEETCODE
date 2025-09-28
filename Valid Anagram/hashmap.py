d9a99b37a91b9efd98f35e8ab18a5ac12b13b475
Kaviru <kavirunethsara1@outlook.com>
2025-09-28 20:23:11 +0530

    Comment out isAnagram function and related logic for future implementation

--- Valid Anagram/hashmap.py
+++ Valid Anagram/hashmap.py
@@ -1,23 +1,24 @@
 s = "anagram"
 t = "nagacam"
 
-def isAnagram(s, t):
-    sCount, tCount = {}, {}
+# def isAnagram(s, t):
+#     sCount, tCount = {}, {}
 
-    if len(s) != len(t):
-        return False
+#     if len(s) != len(t):
+#         return False
 
-    for i in range(len(s)):
-        sCount[s[i]] = sCount.get(s[i], 0) + 1
-        tCount[t[i]] = tCount.get(t[i], 0) + 1
+#     for i in range(len(s)):
+#         sCount[s[i]] = sCount.get(s[i], 0) + 1 #make a dictionary of chr cpunt ex: {"a":1, "b":1} and if find other one add 1 simpily cound the same chrs
+#         tCount[t[i]] = tCount.get(t[i], 0) + 1
 
-    
+#     # for j in sCount:
+#     #     if 
 
-    # print(sCount, tCount)  
+#     print(sCount, tCount)  
 
 
-isAnagram(s, t)
-print(isAnagram(s, t))
+# isAnagram(s, t)
+# print(isAnagram(s, t))
 
 # a = {}
 
@@ -25,3 +26,6 @@ print(isAnagram(s, t))
 # a['a'] = 1
 
 # print(a)
+
+a = {'a': 3, 'n': 1, 'g': 1, 'r': 1, 'm': 1} 
+b = {'n': 1, 'a': 3, 'g': 1, 'c': 1, 'm': 1}
