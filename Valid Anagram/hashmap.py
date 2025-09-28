e52b4273f13a684970a5943c958b1795c86a0fb6
Kaviru <kavirunethsara1@outlook.com>
2025-09-28 20:17:00 +0530

    Refactor isAnagram function to improve character count logic and maintain structure

--- Valid Anagram/hashmap.py
+++ Valid Anagram/hashmap.py
@@ -4,15 +4,20 @@ t = "nagacam"
 def isAnagram(s, t):
     sCount, tCount = {}, {}
 
+    if len(s) != len(t):
+        return False
+
     for i in range(len(s)):
         sCount[s[i]] = sCount.get(s[i], 0) + 1
-        tCount[t[i]] = sCount.get(t[i], 0) + 1
+        tCount[t[i]] = tCount.get(t[i], 0) + 1
 
-    print(sCount, tCount)  
+    
+
+    # print(sCount, tCount)  
 
 
 isAnagram(s, t)
-
+print(isAnagram(s, t))
 
 # a = {}
 
