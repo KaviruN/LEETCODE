000362c060566c7bc4cd60de90dac23676d0266a
Kaviru <kavirunethsara1@outlook.com>
2025-09-29 21:42:24 +0530

    Clean up towSum function by removing commented code and unnecessary print statement

--- Two Sum/main.py
+++ Two Sum/main.py
@@ -5,9 +5,8 @@ target = 7
 def towSum(nums, target):
     numhash = {} #val:index
     for i, n in enumerate(nums):
-        print(i)
-        #  numhash[n] = i
-    # print(numhash)
+        numhash[n] = i
+    print(numhash)
 
 
 towSum(nums, target)
