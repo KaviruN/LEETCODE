223164ff10d34f1d27a77e187d057ebcc5f6f075
Kaviru <kavirunethsara1@outlook.com>
2025-09-29 21:42:57 +0530

    Add detailed comment for numhash initialization in towSum function

--- Two Sum/main.py
+++ Two Sum/main.py
@@ -3,10 +3,9 @@ nums = [3,4,5,6]
 target = 7
 
 def towSum(nums, target):
-    numhash = {} #val:index
+    numhash = {} #val:index {3: 0, 4: 1, 5: 2, 6: 3}
     for i, n in enumerate(nums):
         numhash[n] = i
-    print(numhash)
 
 
 towSum(nums, target)
