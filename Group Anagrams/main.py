ffa33d01460ea9271378505291273fd1c1f1017b
Kaviru <kavirunethsara1@outlook.com>
2025-09-30 19:22:36 +0530

    Comment out groupAnagrams function implementation and test code

--- Group Anagrams/main.py
+++ Group Anagrams/main.py
@@ -1,12 +1,16 @@
 strs = ["act","pots","tops","cat","stop","hat"]
 
-def groupAnagrams(strs):
-    strsCount = {}
+# def groupAnagrams(strs):
+#     strsCount = {}
 
-    for i in range(len(strs)):
-        for char in range(len(strs)):
-            strsCount[strs[i]] = [strs[i]]
-    print(strsCount)
+#     for i in range(len(strs)):
+#         for char in range(len(strs)):
+#             strsCount[strs[i]] = [strs[i]]
+#     print(strsCount)
 
 
-groupAnagrams(strs)
+# groupAnagrams(strs)
+
+a = {"abc":{'a':1}}
+
+print(a["abc"]['a'])
