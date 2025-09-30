a008b602bbfeb0832c01cbdb2904fad176b2c5e9
Kaviru <kavirunethsara1@outlook.com>
2025-09-30 19:53:45 +0530

    Implement groupAnagrams function to count character occurrences in strings

--- Group Anagrams/main.py
+++ Group Anagrams/main.py
@@ -1,16 +1,24 @@
-strs = ["act","pots","tops","cat","stop","hat"]
+strs = ["act","cat"]
 
-# def groupAnagrams(strs):
-#     strsCount = {}
+def groupAnagrams(strs):
+    strsCount = {}
 
-#     for i in range(len(strs)):
-#         for char in range(len(strs)):
-#             strsCount[strs[i]] = [strs[i]]
-#     print(strsCount)
+    for i in range(len(strs)):
+        for char in range(len(strs[i])):
+            if strs[i] not in strsCount:
+                strsCount[strs[i]] = {}
+            if strs[i][char] in strsCount[strs[i]]:
+                strsCount[strs[i]][strs[i][char]] += 1
+            else:
+                strsCount[strs[i]][strs[i][char]] = 1
+
+                
+        
+    print(strsCount)
 
 
-# groupAnagrams(strs)
+groupAnagrams(strs)
 
-a = {"abc":{'a':1}}
+# a = {"abc":{'a':1}, "bcd":{'b':1...}}
 
-print(a["abc"]['a'])
+# print(a["abc"]['a'])
