09c8ad4e38f33eefc1f345e2253cd7ae7c7f9059
Kaviru <kavirunethsara1@outlook.com>
2025-10-01 20:18:31 +0530

    Fix frequency list initialization comment and correct result accumulation in topKelements function

--- Top K Frequent Elements/main.py
+++ Top K Frequent Elements/main.py
@@ -3,7 +3,7 @@ k = 2
 
 def topKelements(nums, k):
     hashmap = {}#1:3,2:2,3:1
-    freq = [[] for i in range(len(nums) + 1)] #[[], [], [], [], [], [], []]
+    freq = [[] for i in range(len(nums) + 1)] #[[], [], [], [], [], [], []] -> [[], [3], [2], [1], [], [], []] 
     for n in nums:
         hashmap[n] = hashmap.get(n, 0) + 1
     for n, c in hashmap.items():
@@ -12,8 +12,8 @@ def topKelements(nums, k):
     res = []
 
     for i in range(len(freq) - 1, 0, -1):
-        for n in freq[n]:
-            res.append(freq[n])
+        for num in freq[i]:
+            res.append(num)
             if len(res) == k:
                 return res
 
