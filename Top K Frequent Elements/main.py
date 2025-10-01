d74bf3e4151bba4258c1868fe6cbf6fc08b37d15
Kaviru <kavirunethsara1@outlook.com>
2025-10-01 20:01:11 +0530

    Add initial implementation of topKelements function to find top K frequent elements

---
+++ Top K Frequent Elements/main.py
@@ -0,0 +1,11 @@
+nums = [1,1,1,2,2,3]
+k = 2
+
+def topKelements(nums, k):
+    hashmap = {}
+    freq = [[] for i in range(len(nums) + 1)] #[[], [], [], [], [], [], []]
+    print(freq)
+
+
+
+topKelements(nums, k)
