06cf03f06b51f3377e7dc8b4ca7d07aa8ae32c34
Kaviru <kavirunethsara1@outlook.com>
2025-10-01 20:05:24 +0530

    Refactor topKelements function to include frequency counting and update print statements

--- Top K Frequent Elements/main.py
+++ Top K Frequent Elements/main.py
@@ -2,9 +2,11 @@ nums = [1,1,1,2,2,3]
 k = 2
 
 def topKelements(nums, k):
-    hashmap = {}
+    hashmap = {}#1:3,2:2,3:1
     freq = [[] for i in range(len(nums) + 1)] #[[], [], [], [], [], [], []]
-    print(freq)
+    for n in nums:
+        hashmap[n] = hashmap.get(n, 0) + 1
+    print(hashmap)
 
 
 
