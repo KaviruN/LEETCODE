d704f4bba60f257d443fe280efb20743ac18923c
Kaviru <kavirunethsara1@outlook.com>
2026-03-21 12:54:56 +0530

    debugging

--- 6Product of Array Except Self/main.py
+++ 6Product of Array Except Self/main.py
@@ -8,9 +8,10 @@ def a(nums):
         prefixes.append(nums[i] * nums[i + 1])
     for j in range(len(nums) - 1, 0, -1):
         suffixes.append(nums[j] * nums[j - 1])
-    for k in range(nums):
-        answer.append(prefixes[k] * suffixes[k])
-    return answer
+    # for k in range(nums):
+    #     answer.append(prefixes[k] * suffixes[k])
+    # return answer
+    return prefixes,suffixes
 
 
 
