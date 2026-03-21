d5d1e31b3e16aab1397c3ff81003c8823a308ada
Kaviru <kavirunethsara1@outlook.com>
2026-03-21 12:24:47 +0530

    Fix index error in product calculation loop in array function

--- 6Product of Array Except Self/main.py
+++ 6Product of Array Except Self/main.py
@@ -3,8 +3,8 @@ nums = [1,2,3,4]
 def a(nums):
     pre = [1]
     suf = []
-    for i in range(len(nums) + 1):
-        pre.append(nums[i + 1] * nums[i])
+    for i in range(len(nums) - 1):
+        pre.append(nums[i] * nums[i + 1])
     return pre
 
 
