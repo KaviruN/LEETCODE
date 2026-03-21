afa85a4f6456b8d934c0e543f2cc1683a91249db
Kaviru <kavirunethsara1@outlook.com>
2026-03-21 19:03:33 +0530

    trying a better methode

--- 6Product of Array Except Self/main.py
+++ 6Product of Array Except Self/main.py
@@ -1,19 +1,32 @@
 nums = [5, 2, 3]
 
+# def a(nums):
+#     prefixes = [1]
+#     suffixes = [1]#5 , 
+#     answer = []
+#     for i in range(len(nums) - 1):
+#         prefixes.append(nums[i] * prefixes[i])
+#     for j in range(len(nums) - 1, 0, -1):
+#         suffixes.append(nums[j] * suffixes[len(suffixes) - 1])
+#         # print(j)
+#     suffixes = list(reversed(suffixes))
+#     for k in range(len(nums)):
+#         answer.append(prefixes[k] * suffixes[k])
+#     return answer
+#     # return prefixes, list(reversed(suffixes))
+
+
 def a(nums):
-    prefixes = [1]
-    suffixes = [1]#5 , 
+    prefixes = 1
+    suffixes = 1#5 , 
     answer = []
     for i in range(len(nums) - 1):
         prefixes.append(nums[i] * prefixes[i])
-    for j in range(len(nums) - 1, 0, -1):
-        suffixes.append(nums[j] * suffixes[len(suffixes) - 1])
-        # print(j)
-    suffixes = list(reversed(suffixes))
-    for k in range(len(nums)):
-        answer.append(prefixes[k] * suffixes[k])
-    return answer
-    # return prefixes, list(reversed(suffixes))
+    # for j in range(len(nums) - 1, 0, -1):
+    #     suffixes.append(nums[j] * suffixes[len(suffixes) - 1])
+    #     # print(j)
+
+
 
 print(a(nums))
 # a(nums)
