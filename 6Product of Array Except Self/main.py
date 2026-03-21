fddaeff793fc7e275ac9db4b5a9cc3226ffb2972
Kaviru <kavirunethsara1@outlook.com>
2026-03-21 13:05:31 +0530

    Fix prefix calculation in product function

--- 6Product of Array Except Self/main.py
+++ 6Product of Array Except Self/main.py
@@ -5,13 +5,13 @@ def a(nums):
     suffixes = [1]
     answer = []
     for i in range(len(nums) - 1):
-        prefixes.append(nums[i] * nums[i + 1])
-    for j in range(len(nums) - 1, 0, -1):
-        suffixes.append(nums[j] * nums[j - 1])
-    # for k in range(nums):
-    #     answer.append(prefixes[k] * suffixes[k])
-    # return answer
-    return prefixes,suffixes
+        prefixes.append(nums[i] * prefixes[i])
+    # for j in range(len(nums) - 1, 0, -1):
+    #     suffixes.append(nums[j] * nums[j - 1])
+    # # for k in range(nums):
+    # #     answer.append(prefixes[k] * suffixes[k])
+    # # return answer
+    return prefixes #,suffixes
 
 
 
