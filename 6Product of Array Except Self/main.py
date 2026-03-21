8d3c29a53f1cace30fa5cb251d5af9c7847a0918
Kaviru <kavirunethsara1@outlook.com>
2026-03-21 12:47:40 +0530

    Fix suffix array calculation and return value in product function

--- 6Product of Array Except Self/main.py
+++ 6Product of Array Except Self/main.py
@@ -5,9 +5,10 @@ def a(nums):
     suf = [1]
     for i in range(len(nums) - 1):
         pre.append(nums[i] * nums[i + 1])
-    for j in range(len(nums) - 1, -1, -1):
-        suf.insert(0, (nums[i] * nums[i - 1]))
+    for j in range(len(nums) - 1, 0, -1):
+        suf.append(nums[j] * nums[j - 1])
+    return suf
 
 
-# print(a(nums))
-a(nums)
+print(a(nums))
+# a(nums)
